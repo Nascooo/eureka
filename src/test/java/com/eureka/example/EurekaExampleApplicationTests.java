fe6cce1e4bb22e93edef04466ee8f63a337edcc2
aabdelnasser <ahmed.abdelnasser@cegedim.com>
2022-05-31 01:45:12 +0200

    Add Eureka

---
+++ src/test/java/com/eureka/example/EurekaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.example;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaExampleApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
